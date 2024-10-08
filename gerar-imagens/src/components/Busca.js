7f25377915e159f99e356b2576a68541851cbf35
Eduardo Berlatto <eduardoberlatto@gmail.com>
2024-10-07 21:37:20 -0300

    inicio da apostila 7

---
+++ gerar-imagens/src/components/Busca.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Button } from 'primereact/button';
+import { IconField } from "primereact/iconfield";
+import { InputIcon } from "primereact/inputicon";
+import { InputText } from 'primereact/inputtext'
+export default class Busca extends React.Component {
+
+    state = {
+        termoDeBusca: ''
+    }
+
+    onTermoAlterado = (event) => {
+        console.log(event.target.value)
+    }
+
+    render() {
+        return (
+            <div className="flex flex-column">
+                <IconField iconPosition='left'>
+                    <InputIcon
+                        className='pi pi-search' />
+                    <InputText
+                        className='w-full'
+                        onChange={this.onTermoAlterado}
+                        placeholder={this.props.dica} />
+                </IconField>
+                <Button 
+                    className='mt-2'
+                    label='OK'/>
+            </div>
+        )
+    }
+}
+
+Busca.defaultProps = {
+    dica: "Digite algo que deseja ver..."
+}
